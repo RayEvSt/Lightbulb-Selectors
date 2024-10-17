e5f17f948d67ee8140ef93bd40b44a82068b190c
RayEvSt <rachelst@mo.co.uk>
2024-10-17 14:54:12 +0100

    lightbulbs turn on and off, counter counts clicks

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-// Write your code here
+
+let bulb1 = document.querySelector('#lightbulb1');
+let bulb2 = document.querySelector('#lightbulb2');
+let bulb3 = document.querySelector('#lightbulb3');
+
+let h3subtitle = document.querySelector('.subtitle');
+
+let count = 0
+
+bulb1.addEventListener('click', function() {    
+    count+=1
+    let COUNT = count.toString();
+    if (count == 1){
+        h3subtitle.innerHTML= "You've clicked the lights " + COUNT + " time";
+    } else {
+        h3subtitle.innerHTML= "You've clicked the lights " + COUNT + " times";
+    }
+    bulb1.classList.toggle("active");
+})
+
+bulb2.addEventListener('click', function() {    
+    count+=1
+    let COUNT = count.toString();
+    if (count == 1){
+        h3subtitle.innerHTML= "You've clicked the lights " + COUNT + " time";
+    } else {
+        h3subtitle.innerHTML= "You've clicked the lights " + COUNT + " times";
+    }
+    bulb2.classList.toggle("active");
+})
+
+bulb3.addEventListener('click', function() {    
+    count+=1
+    let COUNT = count.toString();
+    if (count == 1){
+        h3subtitle.innerHTML= "You've clicked the lights " + COUNT + " time";
+    } else {
+        h3subtitle.innerHTML= "You've clicked the lights " + COUNT + " times";
+    }
+    bulb3.classList.toggle("active");
+})
+
